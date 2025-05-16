f6c57d357821e2aabf8e34a9a8eb1d5ea19c1f4d
lazypool <lazypool@proton.me>
2025-05-16 12:58:54 +0800

    update: mktemp

--- public/help/scripts/change-source/change-source.sh
+++ public/help/scripts/change-source/change-source.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 source "$(pwd)/common.sh"
 
+# 设置临时目录
+TMP_DIR=$(mktemp -d)
+cleanup() {
+	rm -rf "$TMP_DIR"
+}
+trap cleanup EXIT
+
+# 下载必要临时文件
+download() {
+	if ! curl -fsSL "https://${MIRROR_URL}/distros.zip" -o "${TMP_DIR}/distros.zip"; then
+		error_exit "无法下载资源文件" "Failed to download resources"
+	fi
+	if ! unzip -q "${TMP_DIR}/distros.zip" -d "${TMP_DIR}"; then
+		error_exit "解压资源文件失败" "Failed to extract resources"
+	fi
+}
+
 # 分发到对应的发行版脚本
 dispatch() {
 	source /etc/os-release
@@ -65,4 +82,5 @@ if [ $EUID -ne 0 ]; then
 	error_exit "需要 root 权限，请使用 sudo 执行" "Requires root privileges, please use sudo"
 fi
 
+download
 dispatch
